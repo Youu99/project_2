d22ae2652b5683598e9b44bb8cb283b09b847544
Youu99 <154948743+Youu99@users.noreply.github.com>
2024-01-31 17:38:44 +0900

    2024-01-31 마지막업데이트

--- js/main.js
+++ js/main.js
@@ -42,3 +42,28 @@ $(function(){
  
 })
 
+$(function(){
+    var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        // autoplay: {
+        //     delay: 2500,
+        //     disableOnInteraction: false,
+        //   },
+          loop: true,
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable:true,
+        },
+      });
+})
+
