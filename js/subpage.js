b9fc72653d33f860be235051a30f329697067b75
Youu99 <154948743+Youu99@users.noreply.github.com>
2024-02-06 17:37:53 +0900

    2024-02-26 마무리작업

--- js/subpage.js
+++ js/subpage.js
@@ -2,11 +2,16 @@ $(document).ready(function () {
     $(".hamburger").click(function () {
         $(this).toggleClass("is-active");
     });
-
-    $(".buttonicon").click(function(){
-        let idx = $(this).index();
-
-        $(".buttonicon").removeClass("pix1");
-        $(this).addClass("pix1");
-    });
+    
 });
+
+$(window).scroll(function(){
+    let wsc = $(this).scrollTop();
+    console.log(wsc)
+
+    $("#sub_page_img").css("opacity",1-(900/wsc)/1),
+    $("#pc").css("opacity",(1400-wsc)/500),
+    $("#p_pc").css("opacity",1-(900/wsc)/100)
+    
+})
+
